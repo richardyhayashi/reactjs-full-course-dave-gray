4cb497edf6a9db3dcbc2101c1c2c415bfb595ed0
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-05-09 16:30:57 +0900

    Finished Chapter 10

---
+++ tut01/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header({ title = "Default Title"}) {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ tut10/src/components/Square.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Square = ({ colorValue = "Empty Color Value", hexValue, isDarkText }) => {
+  return (
+    <section
+      className='square'
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? "#000" : "#FFF"
+       }}
+    >
+      <p>{colorValue ? colorValue : "Empty Value" }</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
+  );
+};
+
+export default Square;
